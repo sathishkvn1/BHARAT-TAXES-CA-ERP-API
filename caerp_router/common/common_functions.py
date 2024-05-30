a03ce86a7728ac4f8158cf4d0fad3ee5edc5b2c6
AparnaGitHub64 <142775231+AparnaGitHub64@users.noreply.github.com>
2024-05-30 15:08:07 +0600

    1 file updated
    1 file updated

--- caerp_router/common/common_functions.py
+++ caerp_router/common/common_functions.py
@@ -6,7 +6,7 @@ from caerp_db.common.models import Employee
 from caerp_db.database import get_db
 from caerp_db.office import db_office_master
 
-from caerp_db.office.models import AppBusinessConstitution, AppHsnSacClasses, AppHsnSacMaster, AppStockKeepingUnitCode, OffAppointmentCancellationReason, OffAppointmentMaster, OffAppointmentStatus, OffDocumentDataMaster, OffServiceGoodsCategory, OffServiceGoodsGroup, OffServiceGoodsSubCategory, OffServiceGoodsSubGroup
+from caerp_db.office.models import AppBusinessConstitution, AppHsnSacClasses, AppHsnSacMaster, AppStockKeepingUnitCode, OffAppointmentCancellationReason, OffAppointmentMaster, OffAppointmentStatus, OffDocumentDataMaster, OffDocumentDataType, OffServiceGoodsCategory, OffServiceGoodsGroup, OffServiceGoodsSubCategory, OffServiceGoodsSubGroup
 
 from caerp_auth import oauth2
 
@@ -38,7 +38,8 @@ TABLE_MODEL_MAPPING = {
     "AppHsnSacMaster":AppHsnSacMaster,
     "AppStockKeepingUnitCode":AppStockKeepingUnitCode,
     "AppBusinessConstitution":AppBusinessConstitution,
-    "OffDocumentDataMaster":OffDocumentDataMaster
+    "OffDocumentDataMaster":OffDocumentDataMaster,
+    "OffDocumentDataType":OffDocumentDataType
 }
 
 # Define a function to get the model class based on the provided model name
