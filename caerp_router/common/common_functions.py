28d57b3a78ad881e93bf60e012570ac845e30d6f
AparnaGitHub64 <aparnagithub64@gmail.com>
2024-10-01 16:28:08 +0530

    1 file updated
    1 file updated

--- caerp_router/common/common_functions.py
+++ caerp_router/common/common_functions.py
@@ -9,7 +9,7 @@ from caerp_db.database import get_db
 from caerp_db.hr_and_payroll.model import EmployeeTeamMaster, HrDepartmentMaster, HrDesignationMaster, HrDocumentMaster, HrEmployeeCategory, PrlCalculationFrequency, PrlCalculationMethod, PrlSalaryComponent
 from caerp_db.office import db_office_master
 
-from caerp_db.office.models import AppBusinessConstitution, AppDayOfWeek, AppHsnSacClasses, AppHsnSacMaster, AppStockKeepingUnitCode, OffAppointmentCancellationReason, OffAppointmentMaster, OffAppointmentStatus, OffConsultationMode, OffDocumentDataCategory, OffDocumentDataMaster, OffDocumentDataType, OffEnquirerType, OffEnquiryStatus, OffNatureOfPossession, OffServiceGoodsCategory, OffServiceGoodsGroup, OffServiceGoodsMaster, OffServiceGoodsSubCategory, OffServiceGoodsSubGroup, OffSourceOfEnquiry, OffTaskPriority, OffTaskStatus, OffWorkOrderStatus
+from caerp_db.office.models import AppBusinessConstitution, AppDayOfWeek, AppHsnSacClasses, AppHsnSacMaster, AppStockKeepingUnitCode, OffAppointmentCancellationReason, OffAppointmentMaster, OffAppointmentStatus, OffConsultationMode, OffDocumentDataCategory, OffDocumentDataMaster, OffDocumentDataType, OffEnquirerType, OffEnquiryStatus, OffNatureOfPossession, OffServiceDocumentDataDetails, OffServiceGoodsCategory, OffServiceGoodsGroup, OffServiceGoodsMaster, OffServiceGoodsSubCategory, OffServiceGoodsSubGroup, OffSourceOfEnquiry, OffTaskPriority, OffTaskStatus, OffWorkOrderStatus
 
 
 from caerp_auth import oauth2
@@ -77,7 +77,8 @@ TABLE_MODEL_MAPPING = {
     "EmployeeDocuments" :EmployeeDocuments,
     "EmployeeTeamMaster":EmployeeTeamMaster,
     "GstReasonToObtainRegistration":GstReasonToObtainRegistration,
-    "GstTypeOfRegistration":GstTypeOfRegistration
+    "GstTypeOfRegistration":GstTypeOfRegistration,
+    "OffServiceDocumentDataDetails":OffServiceDocumentDataDetails
 
 }
 
